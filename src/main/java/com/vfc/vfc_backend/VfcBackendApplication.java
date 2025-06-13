e9b79cd574cad9c4db7d9a2d82b39cc41ef015d0
Conor Daly <c.daly65@universityofgalway.ie>
2025-06-13 17:29:29 +0100

    Add basic initializer

--- src/main/java/com/vfc/vfc_backend/VfcBackendApplication.java
+++ src/main/java/com/vfc/vfc_backend/VfcBackendApplication.java
@@ -1,10 +1,14 @@
 package com.vfc.vfc_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VfcBackendApplication {
+
+	@PostConstruct
+	public void initialise() {}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VfcBackendApplication.class, args);
