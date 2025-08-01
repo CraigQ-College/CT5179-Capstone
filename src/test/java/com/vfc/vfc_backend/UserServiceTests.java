b7ece00bf2824f2795800d516e5e66759c752820
Conor Daly <c.daly65@universityofgalway.ie>
2025-08-01 15:55:02 +0100

    Added user ID tests

--- src/test/java/com/vfc/vfc_backend/UserServiceTests.java
+++ src/test/java/com/vfc/vfc_backend/UserServiceTests.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
@@ -36,7 +37,9 @@ public class UserServiceTests {
     @BeforeEach
     public void setUp() {
         testUser = new User(TEST_NAME, TEST_EMAIL, TEST_PASSWORD);
+        testUser.setUserId(0);
     }
+
     @Test
     public void findUserIfEmailExists(){
         when(userRepository.findByUserEmail(testUser.getUserEmail())).thenReturn(Optional.ofNullable(testUser));
@@ -51,6 +54,21 @@ public class UserServiceTests {
         User result = userServiceImpl.findByUseremail(TEST_EMAIL);
 
         assert result == null;
+    }
+
+    @Test
+    public void findUserIfIdExists(){
+        when(userRepository.findById(testUser.getUserId())).thenReturn(Optional.ofNullable(testUser));
+        User result = userServiceImpl.findById(0);
+
+        assert result.equals(testUser);
+    }
+
+    @Test
+    public void returnNullUserIfIdDoesNotExist(){
+        when(userRepository.findById(testUser.getUserId())).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class,() -> userServiceImpl.findById(0) );
     }
 
 }
