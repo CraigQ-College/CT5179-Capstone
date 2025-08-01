7a2dc861be4f2bc1dbcf1181d8b3e1b2ad30e9ef
Conor Daly <c.daly65@universityofgalway.ie>
2025-08-01 15:59:48 +0100

    Added user ID tests

--- src/test/java/com/vfc/vfc_backend/UserServiceTests.java
+++ src/test/java/com/vfc/vfc_backend/UserServiceTests.java
@@ -11,10 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
