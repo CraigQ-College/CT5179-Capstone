0936b135f4da631b6d9109775c2ff77c092138c8
Conor Daly <c.daly65@universityofgalway.ie>
2025-08-06 14:38:02 +0100

    Updating tests in Jenkins

--- src/test/java/com/vfc/vfc_backend/VfcBackendApplicationTest.java
+++ src/test/java/com/vfc/vfc_backend/VfcBackendApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VfcBackendApplicationTests {
+class VfcBackendApplicationTest {
 
 	@Test
 	void contextLoads() {
